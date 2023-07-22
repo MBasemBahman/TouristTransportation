19f4a43588892c98c345594f915471757b7eb606
Abdelrahman Osama <43468933+eslamboully@users.noreply.github.com>
2023-07-22 14:39:12 +0300

    ...

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -2,15 +2,6 @@ using System.Runtime.InteropServices;
 
 TenantConfig config = new(TenantEnvironments.Development);
 
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-{
-    LogManager.Configuration.Variables["mydir"] = "c:\\logs\\";
-}
-else
-{
-    LogManager.Configuration.Variables["mydir"] = "/tmp/";
-}
-
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), config.NlogConfig));
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
